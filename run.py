d89971fd5ebe5155c3088b476eae02081e76c846
olliesharp5 <>
2023-12-03 09:27:47 +0000

    Amended get_program_choice to re-prompt consumer

--- run.py
+++ run.py
@@ -19,24 +19,28 @@ data = results.get_all_values()
 
 print(data)
 """
-def get_program_choice():
-    choice = input(
-        "To submit data press 'a', to view the statistics press 'b':\n")
 
-    if choice == 'a':
-        print("Starting survey...")
-        #get_survey_data()
-    elif choice == 'b':
-        print("Fetching statistics...")
-        #get_statistics()
-    else: 
-        print("Invalid choice. Please try again.")
+
+def get_program_choice():
+    while True:
+        choice = input("To submit data press 'a', to view the statistics press 'b':\n")
+
+        if choice == 'a':
+            print("Starting survey...")
+            # get_survey_data()
+            break
+        elif choice == 'b':
+            print("Fetching statistics...")
+            # get_statistics()
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
 
 print("Welcome to the Panem national population survey.")
 get_program_choice()
 
 
-#get_survey_data()
+# get_survey_data()
 
-#get_statistics()
+# get_statistics()
