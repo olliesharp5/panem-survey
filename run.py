2d73cc05f865f7a42743150895c0aea626cf7833
olliesharp5 <>
2023-12-03 09:19:08 +0000

    Create get_program_choice function, print start message

--- run.py
+++ run.py
@@ -5,15 +5,38 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('panem_survey')
 
+""" use to print worksheet data in terminal 
 results = SHEET.worksheet('results')
 
 data = results.get_all_values()
 
 print(data)
+"""
+def get_program_choice():
+    choice = input(
+        "To submit data press 'a', to view the statistics press 'b':\n")
+
+    if choice == 'a':
+        print("Starting survey...")
+        #get_survey_data()
+    elif choice == 'b':
+        print("Fetching statistics...")
+        #get_statistics()
+    else: 
+        print("Invalid choice. Please try again.")
+
+
+print("Welcome to the Panem national population survey.")
+get_program_choice()
+
+
+#get_survey_data()
+
+#get_statistics()
